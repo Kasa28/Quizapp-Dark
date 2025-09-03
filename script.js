56d0b34e03c86e8709a67692f4190f43b8e6bbbb
Kasa28 <agliullinaksenija@gmail.com>
2025-09-03 17:56:15 +0200

    update

--- script.js
+++ script.js
@@ -31,28 +31,29 @@ let questions = [
         "answer_4": "Er gibt eine Nachricht in der Konsole aus",
         "right_answer": 4
     },
+
     {
         "question": "Wie beginnt ein HTML-Dokument?",
-        "answer_1": "<html>",
-        "answer_2": "<body>",
-        "answer_3": "<!DOCTYPE html>",
-        "answer_4": "<head>",
+        "answer_1": "&#60;html&#62;",
+        "answer_2": "&#60;body&#62;",
+        "answer_3": "&#60;!DOCTYPE html&#62;",
+        "answer_4": "&#60;head&#62;",
         "right_answer": 3
     },
     {
         "question": "Welche HTML-Tags benutzt man für Überschriften?",
-        "answer_1": "<headline>",
-        "answer_2": "<heading>",
-        "answer_3": "<h1> bis <h6>",
-        "answer_4": "<head>",
+        "answer_1": "&#60;headline&#62;",
+        "answer_2": "&#60;heading&#62;",
+        "answer_3": "&#60;h1&#62; bis &#60;h6&#62;",
+        "answer_4": "&#60;head&#62;",
         "right_answer": 3
     },
     {
         "question": "Wie bindet man eine CSS-Datei in HTML ein?",
-        "answer_1": "<css>style.css</css>",
-        "answer_2": "<link rel='stylesheet' href='style.css'>",
-        "answer_3": "<script src='style.css'>",
-        "answer_4": "<style src='style.css'>",
+        "answer_1": "&#60;css&#62;style.css&#60;/css&#62;",
+        "answer_2": "&#60;link rel='stylesheet' href='style.css'&#62;",
+        "answer_3": "&#60;script src='style.css'&#62;",
+        "answer_4": "&#60;style src='style.css'&#62;",
         "right_answer": 2
     },
     {
@@ -81,7 +82,9 @@ let questions = [
     }
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;//beginnen mit erste Frage
+
 
 
 
@@ -95,6 +98,18 @@ function init() {
 
 
 function showQuestion() {
+
+    if (currentQuestion >= questions.length) { //currentQuestioon hat nummer 10 am ende weil so viele fragen sind
+    document.getElementById('endscreen').style = '';
+    document.getElementById('questionbody').style = 'display:none';
+     document.getElementById('amount-of-questions').innerHTML = questions.length;
+     document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+     document.getElementById('header-image').src = 'img/winner.jpg'//bild ersetzen
+
+
+    }else{
+        document.getElementById('numberquestion').innerHTML = currentQuestion + 1;
+
     let question = questions[currentQuestion];//machen container daraus holen wir aus questions die 1 frage
 
     document.getElementById('questiontext').innerHTML = question['question'];
@@ -102,7 +117,44 @@ function showQuestion() {
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
-    document.getElementById('answer_5').innerHTML = question['answer_5'];
+    }
 
 }
 
+function answer(selection){
+     let question = questions[currentQuestion];
+     let SelectedQuestionNumber = selection.slice(-1);
+     let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+
+        if(SelectedQuestionNumber == question['right_answer']) { //richtig beantwortrt
+            console.log('Richtig');
+            document.getElementById(selection).parentNode.classList.add('bg-success');
+            rightQuestions++;
+        }else {
+             document.getElementById(selection).parentNode.classList.add('bg-danger');
+             document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+
+        }
+
+        document.getElementById('next').disabled = false;//Nächste Frage button kann gedrückt werden
+    }
+
+    function nextQuestion() {
+        currentQuestion++;//nächste frage 
+        document.getElementById('next').disabled = true; //das der button nächste frage wieder deaktiviert wird
+        resetAnswerButtons();
+        showQuestion();
+    }
+
+
+    function resetAnswerButtons() {
+         document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+         document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+         document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+         document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+         document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+         document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+         document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+         document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    }
